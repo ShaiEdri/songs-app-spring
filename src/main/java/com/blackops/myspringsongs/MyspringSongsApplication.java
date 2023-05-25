c7f17251a17ad64209dfd52ecd9ef3e6913da9c3
shaiedri <shaiedri850@gmail.com>
2023-05-25 10:33:29 +0300

    init

---
+++ src/main/java/com/blackops/myspringsongs/MyspringSongsApplication.java
@@ -0,0 +1,13 @@
+package com.blackops.myspringsongs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringSongsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringSongsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackops/myspringsongs/MyspringSongsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackops.myspringsongs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringSongsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
